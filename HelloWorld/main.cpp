3fb48713506f18c2d505337cff3b13f2a9ac5399
Abdul Fatah <abdulfatah66@gmail.com>
2019-06-19 16:15:28 +0500

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Abdul Fatah on 19/06/2019.
+//  Copyright © 2019 Abdul Fatah. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
